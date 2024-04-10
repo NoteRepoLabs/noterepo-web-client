00aca3d74bd0b988df629ef4524f94e93df191c5
dev-xero <elo48858@gmail.com>
2024-04-10 17:59:20 +0100

    feat: Implement proto reset password page

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+   return (
+      <section className="max-w-2xl md:my-8 mx-3 md:mx-auto flex flex-col justify-center items-center min-h-screen md:h-auto">
+         <h1 className="font-black text-3xl mb-6 text-neutral-900 dark:text-neutral-100">
+            Reset Your Password
+         </h1>
+         <p className="text-center font-bold text-base text-neutral-500 dark:text-neutral-300 mb-12">
+            You&apos;re on this page because you chose to reset your password.
+            Enter a new one to continue.
+         </p>
+      </section>
+   )
+}
