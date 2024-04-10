4a90d5cca9ec7e95da15157ab3d6dcb7c71c9873
dev-xero <elo48858@gmail.com>
2024-04-10 17:25:55 +0100

    feat: Implement verification page

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-   return <h1>Verify Your Email</h1>
+   return (
+      <section className="max-w-2xl md:my-8 mx-3 md:mx-auto flex flex-col justify-center items-center min-h-screen md:h-auto">
+         <h1 className="font-black text-3xl mb-6 text-neutral-900 dark:text-neutral-100">
+            Verify Your Email
+         </h1>
+         <p className="text-center font-bold text-base text-neutral-500 dark:text-neutral-300 mb-12">
+            We&apos;ve sent you a link to verify your email address. Please check your inbox.
+         </p>
+      </section>
+   )
 }
