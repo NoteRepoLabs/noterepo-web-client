d3afeacb2532a6329d8e612f231f40ae327f42cb
dev-xero <elo48858@gmail.com>
2024-06-16 00:37:22 +0100

    feat: Implement sign-out route

---
+++ app/signout/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { deleteCookie } from 'cookies-next';
+import { useEffect } from 'react';
+
+export default function Page() {
+    useEffect(() => {
+        deleteCookie('accessToken');
+        deleteCookie('refreshToken');
+        deleteCookie('user');
+
+        console.log('Deleted cookies successfully.');
+        window.location.href = '/signup';
+    }, []);
+
+    return (
+        <>
+            <section className="mt-8 w-full max-w-lg mx-auto text-center">
+                <h1 className="font-black text-3xl mb-6 text-neutral-900 dark:text-neutral-100">
+                    Sign Out
+                </h1>
+                <p className="text-center font-bold text-base text-neutral-500 dark:text-neutral-300 mb-12">
+                    We&apos;s signing you out of your account, hope to see you
+                    soon.
+                </p>
+            </section>
+        </>
+    );
+}
