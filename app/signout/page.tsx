4624579e7940d4fb5cb9ed7767184dddfa805e64
dev-xero <elo48858@gmail.com>
2024-06-18 21:51:45 +0100

    chore: Change sign out post route to sign in

--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -10,14 +10,14 @@ export default function Page() {
         localStorage.removeItem('user');
 
         console.log('Deleted cookies successfully.');
-        window.location.href = '/signup';
+        window.location.href = '/signin';
     }, []);
 
     return (
         <>
             <section className="mt-8 w-full max-w-lg mx-auto text-center">
                 <h1 className="font-black text-3xl mb-6 text-neutral-900 dark:text-neutral-100">
-                    Sign Out
+                    Signing Out
                 </h1>
                 <p className="text-center font-bold text-base text-neutral-500 dark:text-neutral-300 mb-12">
                     We&apos;s signing you out of your account, hope to see you
