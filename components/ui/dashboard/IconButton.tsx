99c133e14f4817a3660de9e63e820b82699d6eba
dev-xero <elo48858@gmail.com>
2024-06-18 20:52:01 +0100

    chore: Change add repo button color

--- components/ui/dashboard/IconButton.tsx
+++ components/ui/dashboard/IconButton.tsx
@@ -15,8 +15,8 @@ export default function IconButton(props: IconButtonProps) {
                 className={`bg-neutral-900 dark:bg-neutral-100 flex py-3 px-4 rounded-xl space-x-2 hover:bg-neutral-800 dark:hover:bg-neutral-200 ${props.className} max-w-[160px]`}
                 style={{ ...props.style }}
             >
-                <AddSquare size="24" variant="Bold" className='text-neutral-100 dark:text-neutral-900' />
-                <span className="font-extrabold text-md text-neutral-200 dark:text-neutral-900">
+                <AddSquare size="24" variant="Bold" className='text-neutral-100 dark:text-neutral-800' />
+                <span className="font-extrabold text-md text-neutral-200 dark:text-neutral-800">
                     {props.text}
                 </span>
             </button>
