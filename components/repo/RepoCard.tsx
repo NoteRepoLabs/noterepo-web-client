5f072791db6b1c7a8a90ac885e735453ab931cf3
dev-xero <elo48858@gmail.com>
2024-08-11 23:51:16 +0100

    chore: Use neon border for dark theme cards

--- components/repo/RepoCard.tsx
+++ components/repo/RepoCard.tsx
@@ -6,7 +6,7 @@ interface RepoCardProps {
 
 export default function RepoCard(props: RepoCardProps) {
     return (
-        <div className="w-full p-8 rounded-2xl cursor-pointer border-2 border-neutral-200 dark:border-neutral-300 transition-all hover:border-vibrant-green group">
+        <div className="w-full p-8 rounded-2xl cursor-pointer border-2 border-neutral-200 dark:border-neutral-300 transition-all hover:border-vibrant-green dark:hover:border-vibrant-green group">
             <div className="group-hover:translate-y-[-10px] transition-all">
                 <h1 className="text-xl font-extrabold mb-4">{props.repo.name}</h1>
                 <p className="text-neutral-500 dark:text-neutral-300">
