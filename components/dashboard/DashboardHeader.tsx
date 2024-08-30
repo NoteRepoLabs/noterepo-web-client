0c2223e8a9b3a3b7f0fe79d24f9f4363a296189a
dev-xero <elo48858@gmail.com>
2024-08-30 04:11:04 +0100

    chore: Mobile UI improvements

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -20,11 +20,11 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
             <div className="overflow-x-hidden">
                 <header
                     className={`flex flex-col md:flex-row dark:bg-neutral-700 bg-neutral-100 p-4 justify-center border-b-[1.5px] dark:border-b-neutral-800 border-b-neutral-200 md:items-center space-y-4 md:space-y-0 overflow-hidden transition-all md:h-auto fixed z-[999] left-0 right-0 top-0  ${
-                        isExpanded ? 'h-[180px]' : 'h-[70px]'
+                        isExpanded ? 'h-[200px]' : 'h-[70px]'
                     }`}
                 >
                     <div className="w-full max-w-3xl flex flex-col md:flex-row md:justify-between space-y-6 md:space-y-0 gap-2">
-                        <div className=" md:w-auto w-full flex justify-between">
+                        <div className=" md:w-auto w-full flex justify-between items-center">
                             <Logo width={160} height={40} />
                             <div
                                 className="md:hidden cursor-pointer"
@@ -40,9 +40,9 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
                                 }}
                             >
                                 {isExpanded ? (
-                                    <Cancel01Icon size="32" color="#A1A7B5" />
+                                    <Cancel01Icon size={24} color="#A1A7B5" />
                                 ) : (
-                                    <Menu09Icon size="32" color="#A1A7B5" />
+                                    <Menu09Icon size={24} color="#A1A7B5" />
                                 )}
                             </div>
                         </div>
