c21f7a8831b2554bfca3298ff91c9b599d760533
dev-xero <elo48858@gmail.com>
2024-09-23 19:37:09 +0100

    feat: Structure account page

---
+++ app/account/page.tsx
@@ -0,0 +1,30 @@
+/**
+ *  2024 - NoteRepo Engineering, Open Source Software
+ *  This file is part of the source code which is available online.
+ *      - GitHub: https://github.com/NoteRepoLabs/noterepo-web-client
+ *      - LICENSE: MIT
+ */
+
+'use client'
+
+import Container from "@/layout/Container"
+import { ArrowLeft02Icon } from "hugeicons-react"
+
+export default function Page() {
+    return (
+        <>
+            <Container>
+                <a
+                    href="/"
+                    className="flex gap-2 items-center px-2 md:px-0 mt-2 opacity-80 hover:opacity-100 transition-opacity"
+                >
+                    <ArrowLeft02Icon />
+                    <span>Back</span>
+                </a>
+                <header className="mt-2 py-2 border-b dark:border-b-highlight border-neutral-300 mb-4">
+                    <h1 className="text-3xl mb-2 font-bold">Account</h1>
+                </header>
+            </Container>
+        </>
+    )
+}
